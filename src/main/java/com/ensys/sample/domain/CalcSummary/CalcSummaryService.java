0f0eae562e05699b0801dbbeaa4e1d220311a729
KangMin <KangMin@DESKTOP-6B5AEQD>
2020-08-03 00:17:14 +0900

    정산관리 승인버튼 추가

--- src/main/java/com/ensys/sample/domain/CalcSummary/CalcSummaryService.java
+++ src/main/java/com/ensys/sample/domain/CalcSummary/CalcSummaryService.java
@@ -1,30 +1,42 @@
 package com.ensys.sample.domain.CalcSummary;
-import com.ensys.sample.domain.user.SessionUser;
-import com.ensys.sample.utils.SessionUtils;
-import org.springframework.stereotype.Service;
 
-import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ensys.sample.domain.user.SessionUser;
+import com.ensys.sample.utils.SessionUtils;
 
 @Service
 public class CalcSummaryService {
 
-    @Inject
-    private CalcSummaryMapper mapper;
+	@Inject
+	private CalcSummaryMapper mapper;
 
-    public List<HashMap<String, Object>> selectH(HashMap<String, Object> param) {
-        SessionUser user = SessionUtils.getCurrentUser();
-        param.put("COMPANY_CD",user.getCdCompany());
-        param.put("LOGIN_ID",user.getIdUser());
-        return mapper.selectH(param);
-    }
-    
-    public List<HashMap<String, Object>> selectPop(HashMap<String, Object> param) {
-        SessionUser user = SessionUtils.getCurrentUser();
-        param.put("COMPANY_CD",user.getCdCompany());
-        param.put("LOGIN_ID",user.getIdUser());
-        return mapper.selectPop(param);
-    }
-    
+	public List<HashMap<String, Object>> selectH(HashMap<String, Object> param) {
+		SessionUser user = SessionUtils.getCurrentUser();
+		param.put("COMPANY_CD", user.getCdCompany());
+		param.put("LOGIN_ID", user.getIdUser());
+		return mapper.selectH(param);
+	}
+
+	public List<HashMap<String, Object>> selectPop(HashMap<String, Object> param) {
+		SessionUser user = SessionUtils.getCurrentUser();
+		param.put("COMPANY_CD", user.getCdCompany());
+		param.put("LOGIN_ID", user.getIdUser());
+		return mapper.selectPop(param);
+	}
+
+	@Transactional
+	public void approve(HashMap<String, Object> param) {
+		SessionUser user = SessionUtils.getCurrentUser();
+		param.put("COMPANY_CD", user.getCdCompany());
+		param.put("LOGIN_ID", user.getIdUser());
+		mapper.approve(param);
+	}
+
 }
